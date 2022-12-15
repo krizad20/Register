5187374607a80825fb6fef1d4b08b986cd03053b
krizad20 <vim_intason@hotmail.com>
2022-12-15 18:31:32 +0700

    sent data to sheet

--- script.js
+++ script.js
@@ -18,4 +18,17 @@
             }, false)
         })
 
+    var form = document.getElementById('form');
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        fetch(form.action, {
+            method: "POST",
+            body: new FormData(document.getElementById("form")),
+        }).then(
+            response => response.json()
+        ).then((html) => {
+            // you can put any JS code here
+            alert('ส่งข้อมูลสำเร็จ')
+        });
+    });
 })()
